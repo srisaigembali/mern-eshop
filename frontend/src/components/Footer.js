373f28182757eec8d725863194918bd1ff02fdbb
tensai <tensai@gmail.com>
2023-05-16 18:24:30 +0530

    frontend setup

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-white p-3">
+      <p className="text-center mt-2">All Right Reserved &copy; Srisai</p>
+    </div>
+  );
+};
+
+export default Footer;
