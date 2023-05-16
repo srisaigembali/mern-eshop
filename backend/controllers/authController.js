1a68e522df7a5d2e8dbee102702f26714526d0f0
tensai <tensai@gmail.com>
2023-05-16 12:22:12 +0530

    added login route and json web token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 import userModel from '../models/userModel.js';
-import { hashPassword } from '../utils/authUtil.js';
+import { comparePassword, hashPassword } from '../utils/authUtil.js';
+import JWT from 'jsonwebtoken';
 
+// Post Register
 export const registerController = async (req, res) => {
   try {
     const { name, email, password, phone, address } = req.body;
@@ -50,6 +52,61 @@ export const registerController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: 'Error in registration',
+      error,
+    });
+  }
+};
+
+// Post Login
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validations
+    if (!email || !password) {
+      return res.status(404).send({
+        success: false,
+        message: 'Invalid email or password',
+      });
+    }
+
+    //check for user
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).send({
+        success: true,
+        message: 'Email not registered',
+      });
+    }
+    const matchPassword = await comparePassword(password, user.password);
+    if (!matchPassword) {
+      return res.status(200).send({
+        success: false,
+        message: 'Invalid password',
+      });
+    }
+
+    // create token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRETKEY, {
+      expiresIn: process.env.JWT_EXPIRY,
+    });
+
+    res.status(200).send({
+      success: true,
+      message: 'Login Successful',
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: 'Error in login',
       error,
     });
   }
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { registerController } from '../controllers/authController.js';
+import {
+  registerController,
+  loginController,
+} from '../controllers/authController.js';
 
 // router object
 const router = express.Router();
 
 // routing
 router.post('/register', registerController);
+router.post('/login', loginController);
 
 export default router;
