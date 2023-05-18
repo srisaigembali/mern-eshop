dd299202b54dd24cd180780b2f8cba856ba1101d
tensai <tensai@gmail.com>
2023-05-18 12:24:01 +0530

    added category crud in frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/Auth';
+import 'antd/dist/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +16,4 @@ root.render(
   </AuthProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- frontend/src/pages/Admin/CreateCategory.js
+++ frontend/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AdminMenu from '../../components/AdminMenu';
-import Layout from './../../components/Layout';
+import Layout from '../../components/Layout';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import CategoryForm from '../../components/Forms/CategoryForm';
+import { Modal } from 'antd';
 
 const CreateCategory = () => {
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState('');
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [updatedName, setUpdatedName] = useState('');
+
+  // handle form
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.post('/api/category/create-category', {
+      name,
+    });
+    if (data.success) {
+      toast.success(`${name} is created`);
+      getAllCategories();
+    } else {
+      toast.error(data.message);
+    }
+    try {
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong in category form');
+    }
+  };
+
+  // get all categories
+  const getAllCategories = async () => {
+    try {
+      const { data } = await axios.get('/api/category/get-categories');
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong in getting category');
+    }
+  };
+
+  useEffect(() => {
+    getAllCategories();
+  }, []);
+
+  // update category
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        `/api/category/update-category/${selected._id}`,
+        { name: updatedName }
+      );
+      if (data.success) {
+        toast.success(`${updatedName} is updated`);
+        setVisible(false);
+        setUpdatedName('');
+        setSelected(null);
+        getAllCategories();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong in updating category');
+    }
+  };
+
+  // delete category
+  const handleDelete = async (cid) => {
+    try {
+      const { data } = await axios.delete(
+        `/api/category/delete-category/${cid}`
+      );
+      if (data.success) {
+        toast.success(`category is deleted`);
+        getAllCategories();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error('Something went wrong in updating category');
+    }
+  };
+
   return (
     <Layout title={'Dashboard - Create Category'}>
       <div className="container-fluid m-3 p-3">
@@ -11,7 +98,65 @@ const CreateCategory = () => {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <h1>Create Category</h1>
+            <h1>Manage Category</h1>
+            <div className="p-3 w-50">
+              <CategoryForm
+                handleSubmit={handleSubmit}
+                value={name}
+                setValue={setName}
+              />
+            </div>
+            <div className="w-75">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {categories?.map((category) => (
+                    <>
+                      <tr>
+                        <td key={category._id}>{category.name}</td>
+                        <td>
+                          <button
+                            className="btn btn-primary me-3"
+                            onClick={() => {
+                              setVisible(true);
+                              setUpdatedName(category.name);
+                              setSelected(category);
+                            }}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="btn btn-danger"
+                            onClick={() => {
+                              handleDelete(category._id);
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <Modal
+              onCancel={() => setVisible(false)}
+              footer={null}
+              visible={visible}
+            >
+              <h4>Update category</h4>
+              <CategoryForm
+                value={updatedName}
+                setValue={setUpdatedName}
+                handleSubmit={handleUpdate}
+              />
+            </Modal>
           </div>
         </div>
       </div>
