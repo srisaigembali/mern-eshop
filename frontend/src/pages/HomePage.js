04e3c480ae1416b3d441953e9339f258f2b71c4f
tensai <tensai@gmail.com>
2023-05-18 19:39:54 +0530

    added filter products based on category and price

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,13 +1,194 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
-import { useAuth } from '../context/Auth';
+import axios from 'axios';
+import { Checkbox, Radio } from 'antd';
+import { Prices } from '../components/Prices';
+import { AiOutlineReload } from 'react-icons/ai';
 
 const HomePage = () => {
-  const [auth, setAuth] = useAuth();
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  // get all categories
+  const getAllCategories = async () => {
+    try {
+      const { data } = await axios.get('/api/category/get-categories');
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // get all products
+  const getAllProducts = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`/api/product/product-list/${page}`);
+      setLoading(false);
+      setProducts(data.products);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  //get total Count
+  const getTotal = async () => {
+    try {
+      const { data } = await axios.get('/api/product/product-count');
+      setTotal(data?.total);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // load more
+  const loadMore = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`/api/product/product-list/${page}`);
+      setLoading(false);
+      setProducts([...products, ...data?.products]);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategories();
+    getTotal();
+  }, []);
+
+  useEffect(() => {
+    if (!checked.length || !radio.length) getAllProducts();
+  }, [checked.length, radio.length]);
+
+  useEffect(() => {
+    if (checked.length || radio.length) filterProducts();
+  }, [checked, radio]);
+
+  useEffect(() => {
+    if (page === 1) return;
+    loadMore();
+  }, [page]);
+
+  // filter by category
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+
+  //get filtered product
+  const filterProducts = async () => {
+    try {
+      const { data } = await axios.post('/api/product/product-filters', {
+        checked,
+        radio,
+      });
+      setProducts(data?.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Layout>
-      <div>Home Page</div>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    <Layout title={'All Products - Best Offers'}>
+      <div className="row mt-2">
+        <div className="col-md-2">
+          {/* category filter */}
+          <h4 className="text-center">Filter by category</h4>
+          <div className="d-flex flex-column">
+            {categories.map((c) => (
+              <Checkbox
+                key={c._id}
+                onChange={(e) => {
+                  handleFilter(e.target.checked, c._id);
+                }}
+              >
+                {c.name}
+              </Checkbox>
+            ))}
+          </div>
+          {/* price filter */}
+          <h4 className="text-center mt-4">Filter by price</h4>
+          <div className="d-flex flex-column">
+            <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+              {Prices?.map((p) => (
+                <div key={p._id}>
+                  <Radio value={p.array}>{p.name}</Radio>
+                </div>
+              ))}
+            </Radio.Group>
+          </div>
+          {/* clear filters */}
+          <div className="d-flex flex-column">
+            <button
+              className="btn btn-danger"
+              onClick={() => window.location.reload()}
+            >
+              Reset Filters
+            </button>
+          </div>
+        </div>
+        <div className="col-md-9">
+          <h1 className="text-center">All Products</h1>
+          <div className="d-flex flex-wrap">
+            {products?.map((p) => (
+              <div className="card m-2" style={{ width: '18rem' }}>
+                <img
+                  src={`/api/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">
+                    {p.description.substring(0, 30)}...
+                  </p>
+                  <p className="card-text">$ {p.price}</p>
+                  <button className="btn btn-primary ms-1">More details</button>
+                  <button className="btn btn-secondary ms-1">
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="m-2 p-3">
+            {products && products.length < total && (
+              <button
+                className="btn loadmore"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setPage(page + 1);
+                }}
+              >
+                {loading ? (
+                  'Loading ...'
+                ) : (
+                  <>
+                    {' '}
+                    Loadmore <AiOutlineReload />
+                  </>
+                )}
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
