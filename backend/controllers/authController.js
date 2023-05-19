b3ec3aa46de5a1d8273455894ee9f8d4c6ec6cda
tensai <tensai@gmail.com>
2023-05-19 19:41:46 +0530

    added user orders page

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 import userModel from '../models/userModel.js';
+import orderModel from '../models/orderModel.js';
 import { comparePassword, hashPassword } from '../utils/authUtil.js';
 import JWT from 'jsonwebtoken';
 
@@ -194,6 +195,64 @@ export const updateProfileController = async (req, res) => {
     res.status(400).send({
       success: false,
       message: 'Error while updating` profile',
+      error,
+    });
+  }
+};
+
+// user orders
+export const getOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({ buyer: req.user._id })
+      .populate('products', '-photo')
+      .populate('buyer', 'name');
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: 'Error WHile Geting Orders',
+      error,
+    });
+  }
+};
+
+// all orders
+export const getAllOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({})
+      .populate('products', '-photo')
+      .populate('buyer', 'name')
+      .sort({ createdAt: '-1' });
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: 'Error WHile Geting Orders',
+      error,
+    });
+  }
+};
+
+// order status
+export const orderStatusController = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+    const orders = await orderModel.findByIdAndUpdate(
+      orderId,
+      { status },
+      { new: true }
+    );
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: 'Error While Updateing Order',
       error,
     });
   }
